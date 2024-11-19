4189614824ed88cff56a55f935d5b00e404f919a
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-11-19 13:29:15 -0500

    completed welcome section

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -3,7 +3,7 @@ export default function NavBar() {
     <nav className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-l font-bold">
-          <h1>Japan Budget App</h1>
+          <h1>Japan visualizer app</h1>
         </div>
         <ul className="flex space-x-4">
           <li className="hover:text-gray-400 cursor-pointer">About</li>
