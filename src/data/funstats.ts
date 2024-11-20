dd7f0fdd2086501fe2bce0f9031cdb6383603e71
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-11-20 12:34:19 -0500

    updateed code in the about page

---
+++ src/data/funstats.ts
@@ -0,0 +1,87 @@
+// statsData.ts
+export const statsData = [
+  {
+    id: 1,
+    icon: "🛬",
+    title: "Flight Duration",
+    description: "EWR to NRT",
+    value: "14hours"
+  },
+  {
+    id: 2,
+    icon: "🔥",
+    title: "Most Expensive Meal",
+    description: "Yakiniku",
+    value: "¥5000"
+  },
+  {
+    id: 3,
+    icon: "🚄",
+    title: "Longest Journey",
+    description: "Tokyo to Osaka",
+    value: "515 km"
+  },
+  {
+    id: 4,
+    icon: "🎮🕹️🎶",
+    title: "Most played Arcade Game",
+    description: "Dance Rush Stardom",
+    value: "8 times"
+  },
+  {
+    id: 5,
+    icon: "🧸",
+    title: "Crane game prizes won",
+    description: "7 prizes",
+    value: ""
+  },
+  {
+    id: 6,
+    icon: "🏙️🏯",
+    title: "Favorite City",
+    description: "Osaka",
+    value: ""
+  },
+  {
+    id: 7,
+    icon: "🍺",
+    title: "Favorite Drink",
+    description: "Whiskey Highballs",
+    value: ""
+  },
+  {
+    id: 8,
+    icon: "🌎🎢",
+    title: "Biggest disappointment",
+    description: "Universal Studios",
+    value: "No nintendo world :("
+  },
+  {
+    id: 9,
+    icon: "🏪🍙🥞",
+    title: "Favorite Konbini",
+    description: "Lawsons",
+    value: ""
+  },
+  {
+    id: 10,
+    icon: "🏋️‍♀️",
+    title: "Heaviest Weight Lifted",
+    description: "177.5kg",
+    value: "Deadlift"
+  },
+  {
+    id: 11,
+    icon: "🛍️🎉🏬",
+    title: "Most expensive souvenirs",
+    description: "Donki",
+    value: "¥20000"
+  },
+  {
+    id: 12,
+    icon: "🏙️🛍️🎮",
+    title: "Favorite District",
+    description: "Ikebukuro",
+    value: ""
+  }
+];
