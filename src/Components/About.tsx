18870b135b0a928bff59baeccb64a911f1e625b0
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-11-20 12:19:34 -0500

    doing some layout fixes

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -2,14 +2,19 @@ export default function About() {
   return (
     <>
       <div className='mt-8'>
-        <section id="stats" className="text-center">
-          <h2 className="text-3xl font-bold mb-4">Basic and Fun Stats</h2>
-          <p>At the time of my trip the exchange rate was as follows:</p>
-          <p className='italic'>This is an approximation of the USD to JPY exchange rate</p>
-          <span className="text-4xl">152💴 (JPY)</span> to <span className="text-4xl">1💵 (USD)</span>
+        <section id="stats" className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className='flex justify-center items-center flex-col'>
+            <h2 className="text-3xl font-bold mb-4">Basic and Fun Stats</h2>
+          </div>
+
+          <div className="flex flex-row justify-center items-star gap-x-1">
+            <span className="text-4xl">152💴 (JPY)</span>
+            <span className='text-4xl'>=</span>
+            <span className="text-4xl">1💵 (USD)</span>
+          </div>
         </section>
         <div className="flex gap-4 flex-wrap justify-center">
-        <div className="flex flex-col items-center bg-blue-100 text-blue-700 p-4 rounded-lg shadow">
+          <div className="flex flex-col items-center bg-blue-100 text-blue-700 p-4 rounded-lg shadow">
             <span className="text-4xl">🛬</span>
             <h2 className="text-lg font-semibold mt-2">Flight Duration</h2>
             <p className='text-lg'>EWR to NRT</p>
